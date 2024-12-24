d82ce5b98da239ba0aa385be7791dd0d29f3dab8
WilDev <165216278+WilDev26@users.noreply.github.com>
2024-12-25 06:49:58 +0700

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,8 +1,8 @@
 clear
 echo -e '\e[1;37mUpdate & Upgrade...\e[0m'
 clear
-pkg update
-pkg upgrade
+apt update
+apt upgrade
 clear
 echo -e '\e[1;37mDownload File...\e[0m'
 wget -o wifi-hack.tar.gz https://github.com/WilDev26/wifi-hack/raw/refs/heads/main/wifi-hack.tar.gz
@@ -12,14 +12,12 @@ tar -xvzf wifi-hack.tar.gz
 rm wifi-hack.tar.gz
 clear
 echo -e '\e[1;37mTunggu harap bersabar...\e[0m'
-pkg update
-pkg upgrade
-pkg install tsu
-pkg install wget
-pkg install python
-pkg install git
-pkg install -y root-repo
-pkg install -y git tsu python wpa-supplicant pixiewps iw openssl
+apt install tsu
+apt install wget
+apt install python
+apt install git
+apt install -y root-repo
+apt install -y git tsu python wpa-supplicant pixiewps iw openssl
 termux-setup-storage
 wget -O wifi-hack.sh https://raw.githubusercontent.com/WilDev26/wifi-hack/refs/heads/main/wifi-hack.sh
 chmod +rwx Setup.sh
